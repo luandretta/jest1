259dcf27041599562e5f4ce362485be43752f4b2
Lucimeri Andretta <113333171+luandretta@users.noreply.github.com>
2023-02-16 13:59:01 +0000

    Feat: Test the DOM

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -4,7 +4,12 @@
 const buttonClick = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    // document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 
@@ -13,4 +18,7 @@ describe("DOM tests", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
     });
+    test("h1 should exist", ()=> {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    })
 });
